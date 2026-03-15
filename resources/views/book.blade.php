b8a15e5ef6f8844370882e56bd7e29ab18bdc2d7
rahelaqambari <rahelaqambari@gmail.com>
2026-03-15 20:02:04 +0430

    changes

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -1,3 +1,14 @@
-<div>
-    <!-- The only way to do great work is to love what you do. - Steve Jobs -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1>About Me</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum beatae culpa hic natus aliquid obcaecati soluta vero architecto cupiditate minima eos sed maiores laborum cum fugit, quod sapiente explicabo totam?</p>
+    </div>
+</body>
+</html>
