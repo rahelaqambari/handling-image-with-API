de68f0aaa07cf231f566daf3ddf1556b79d7ca34
rahelaqambari <rahelaqambari@gmail.com>
2026-03-15 11:20:26 +0430

    function update and delete

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\Rule;
 
 class ProductController extends Controller
@@ -15,6 +16,7 @@ class ProductController extends Controller
     public function index()
     {
         //
+
     }
 
     /**
@@ -23,35 +25,35 @@ class ProductController extends Controller
     public function store(Request $request)
     {
         //
-      try{
-         $validate = $request->validate([
+        try{
+       $validate = $request->validate([
             "name"=>["string","required","min:3",
-            Rule::unique("product","name")
+            Rule::unique("products","name")
             ],
-            "price"=>"required|numeric",
-            "image"=> "required|image|mimes:jpg,png,jpeg,gif"
+"price"=>"required|numeric",
+"image"=>"required|image|mimes:jpg,png,jpeg,gif"
         ]);
-        $imagepath = "";
+        $imagepath ="";
         if($request->hasFile("image")){
-            $imagepath = $request->file("image")->store("product-img","public");
+            $imagepath = $request->file("image")->store("products-image","public",);
         }
-        $product = Product::create(
+       $product = Product::create(
             [
-                "name"=>$validate["name"],
-                "price"=>$validate["price"],
-                "image_url"=>$imagepath,
+              "name"=>$validate["name"],
+              "price"=>$validate["price"],
+              "image_url"=>$imagepath 
             ]
         );
         return response()->json([
-            "data"=>$product,
+          "data"=>$product,  
         ]);
-      }
-      catch(Exception $err){
-        return response()->json([
-            "data"=>$err->getMessage()
-        ]);
-
-      }
+        }catch(Exception $err){
+            return response()->json(
+                [
+                    "data"=>$err->getMessage()
+                ]
+            );
+        }
     }
 
     /**
@@ -59,22 +61,54 @@ class ProductController extends Controller
      */
     public function show(string $id)
     {
-        // 
+        //
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Product $product)
     {
         //
+        $request->validate(
+            [
+                "name"=>["string","nullable","min:2","max:30",
+                Rule::unique('products','name')->ignore($product->id)
+                ],
+                "price"=>"nullable|numeric|decimal:0,2",
+                "image"=>"required|image|mimes:jpg,png,jpeg,gif"
+            ],
+            // [messages error]
+        );
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $imagepath="";
+        if($request->hasFile("image")){
+            if($product->image_url && Storage::disk("public")->exists($product->image_url)){
+                Storage::disk("public")->delete($product->image_url);
+            }
+            // $request->file("image")->
+              $imagepath = $request->file("image")->store("products-image","public",);
+              $product->image_url=$imagepath;
+              
+        }
+        $product->update();
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(string $id)
-    {
+       {
         //
+        $product= Product::findOrfail($id);
+         if($product->image_url && Storage::disk("public")->exists($product->image_url)){
+                Storage::disk("public")->delete($product->image_url);
+            }
+            $product->delete();
+            return response()->json(
+               "deleted",
+            );
     }
 }
